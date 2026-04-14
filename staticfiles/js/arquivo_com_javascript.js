ebd98683b152b3f85f835fc1a58ffad474678b2f
Argel dos Santos <argeldossantos@outlook.com>
2026-04-14 02:11:17 -0300

    Conclusão da interface de usabilidade e integração de fotos - Roteiros 5, 6 e 12 finalizados

---
+++ staticfiles/js/arquivo_com_javascript.js
@@ -0,0 +1,9 @@
+function mudarCor() {
+    // Dá um feedback visual imediato ao clicar em Categorias
+    document.body.style.backgroundColor = "lightblue";
+}
+
+function alertarMensagem() {
+    // Avisa o usuário que a funcionalidade está em desenvolvimento
+    alert("Este botão ainda não está funcionando!");
+}
